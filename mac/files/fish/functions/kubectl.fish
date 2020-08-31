a7b69b0b39f615d06ec14386d6838542ceb38ebf
Curtis John <admin@curtisrayjohn.com>
2020-08-31 12:22:40 -0400

    Update kubectl.fish

--- mac/files/fish/functions/kubectl.fish
+++ mac/files/fish/functions/kubectl.fish
@@ -1,3 +1,4 @@
-function k
-        kubectl $argv
+# https://github.com/evanlucas/fish-kubectl-completions/issues/22#issuecomment-505487519
+function kc --wraps kubectl -d 'kubectl shorthand'
+  kubectl $argv
 end
