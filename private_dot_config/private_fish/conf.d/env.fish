4a780ba418db5ce96dd7ee209ead2313edff30d3
Curtis John <admin@curtisrayjohn.com>
2021-04-14 12:12:10 -0400

    setting up direnv

--- private_dot_config/private_fish/conf.d/env.fish
+++ private_dot_config/private_fish/conf.d/env.fish
@@ -1,2 +1,6 @@
 # fix for password-protected GPG keys when signing git commits
 set -gx GPG_TTY (tty)
+# direnv setup
+direnv hook fish | source
+# direnv mode
+set -g direnv_fish_mode disable_arrow
